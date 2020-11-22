17f3db6beda878017850e79a2fe009a1be6416b5
nguyen.dang.van <nguyen.dang.van@sun-asterisk.com>
2020-11-22 13:03:14 +0700

    [] Create controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ApplicationRecord
+	PARAMS_MOVIE = %i(title original_title movie_image movie_type
+	 content status rating aried ended)
+
+	# has_many :genres, through: :movie_genres
+
 	enum movie_type: [:tv_series, :movie]
 	enum satus: [:upcoming, :ongoing, :completed]
 
