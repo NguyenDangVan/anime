98b8278739d8ec5228277013cc29a98d306390fc
nguyen.dang.van <nguyen.dang.van@sun-asterisk.com>
2020-12-21 17:58:46 +0700

    Fix API login

--- app/javascript/actions/userActions.jsx
+++ app/javascript/actions/userActions.jsx
@@ -3,16 +3,25 @@ import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGOUT } from "./types";
 
 export const loginUser = (user) => async (dispatch) => {
 	const login_url = "http://localhost:3001/api/v1/login";
-	dispatch(loginRequest);
-	try {
-		const { data } = await axios.post(login_url, user);
+	// dispatch(loginRequest);
 
-		// localStorage.setItem("token", JSON.stringify(data.token));
-		console.log(loginSuccess(data.user))
-    dispatch(loginSuccess(data.user));
-	} catch (err) {
-		dispatch(loginFailure({ login: err.response.data }))
-	}
+	// try {
+	// 	const { data } = (await axios.post(login_url, user)) || {};
+
+	// 	// localStorage.setItem("token", JSON.stringify(data.token));
+	// 	console.log(loginSuccess(data.user))
+ //    dispatch(loginSuccess(data.user));
+	// } catch (err) {
+	// 	dispatch(loginFailure({ login: err.response.data }));
+	// }
+	axios
+		.post(login_url, user)
+		.then(res => return res.json())
+		.then(res => {
+			console.log(res)
+			dispatch(loginSuccess(res.user))
+		})
+		.catch(err => loginFailure(err))
 }
 
 const loginRequest = () => {
